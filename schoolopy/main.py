d96a7a997202a9b098e73ef7615323181e027417
Sam Terfa <samterfa@gmail.com>
2025-02-23 17:34:52 -0600

    Fix indent

--- schoolopy/main.py
+++ schoolopy/main.py
@@ -2222,11 +2222,11 @@ class Schoology:
         """
         return self._search(keywords, 'course')
 
-def csvexport_users(self, fields):
-    """
-    Export a CSV of user data.
+    def csvexport_users(self, fields):
+        """
+        Export a CSV of user data.
 
-    :param fields: Field headers to be returned. Must be one or more of uid, school_uid, building_nid, name_title, name_first, name_first_preferred, name_middle, name_last, role_name, name, mail, position, grad_year, birthday, gender, bio, subjects_taught, grades_taught, phone, address, website, interests, activities
-    :return: CSV string of user data.
-    """
-    return self._get('csvexport/users', {'fields': ','.join(fields)}, add_start_limit=False)
+        :param fields: Field headers to be returned. Must be one or more of uid, school_uid, building_nid, name_title, name_first, name_first_preferred, name_middle, name_last, role_name, name, mail, position, grad_year, birthday, gender, bio, subjects_taught, grades_taught, phone, address, website, interests, activities
+        :return: CSV string of user data.
+        """
+        return self._get('csvexport/users', {'fields': ','.join(fields)}, add_start_limit=False)
